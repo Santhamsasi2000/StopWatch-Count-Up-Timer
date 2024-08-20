9c56452130f1ce00aed9ebc3c86a01cfef34cf8f
Santhamsasi2000 <santhamsasi68670@gmail.com>
2024-08-20 14:11:28 +0530

    Created Stopwatch UI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='d-flex justify-content-center align-items-center flex-column'>
+       <h1>01-Stopwatch</h1>
+       <p>00:00:00</p>
+       <div className='d-flex gap-5'>``
+         <button>Stop</button>
+         <button>Reset</button>
+       </div>
     </div>
   );
 }
